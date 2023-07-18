b009e8b023b3fec16742873b107d83612f8139a9
vishalicious213 <vishalicious213@gmail.com>
2023-07-17 17:54:35 -0700

    manually rendering Die 10x from App

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,15 @@ export default function App() {
         <main>
             <section className="dice">
                 <Die value={1} />
+                <Die value={2} />
+                <Die value={3} />
+                <Die value={4} />
+                <Die value={5} />
+                <Die value={6} />
+                <Die value={1} />
+                <Die value={2} />
+                <Die value={3} />
+                <Die value={4} />
             </section>
         </main>
     )
