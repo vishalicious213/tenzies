185520686fa463bbdf2b4f396b7f7aa3f0a27cba
vishalicious213 <vishalicious213@gmail.com>
2023-07-17 20:20:26 -0700

    updated code so that if a die's 'isHeld' property is true, its background color is green

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ export default function App() {
         <main>
             <section className="dice-container">
                 {/* spot, below is the index for each die in the array */}
-                {dice.map((die, spot) => <Die value={die.value} key={spot} />)}
+                {dice.map((die, spot) => <Die value={die.value} isHeld={die.isHeld} key={spot} />)}
             </section>
 
             <button className="roll-dice" onClick={rollDice}>Roll</button>
--- src/Die.jsx
+++ src/Die.jsx
@@ -1,6 +1,6 @@
 export default function Die(props) {
     return (
-        <div className="die-face">
+        <div className={props.isHeld ? "die-face held" : "die-face"}>
             <h2 className="die-num">{props.value}</h2>
         </div>
     )
