98bea506bd38574b7357393e951b96ea850b3174
vishalicious213 <vishalicious213@gmail.com>
2023-07-19 12:25:06 -0700

    updating sorted scores right from button onClicks

--- src/Scores.jsx
+++ src/Scores.jsx
@@ -5,27 +5,12 @@ export default function Scores({scores}) {
     const sortedByRolls = [...scoreArray].sort((a, b) => a.roll - b.roll)
     const sortedByTime = [...scoreArray].sort((a, b) => a.time - b.time)
 
-    console.log("sortedByRolls", sortedByRolls)
-    console.log("sortedByTime", sortedByTime)
-
-    console.log("scores", scores)
-
-    function renderByRolls() {
-        setScoreArray(sortedByRolls)
-        console.log("rolls", scoreArray)
-    }
-
-    function renderByTime() {
-        setScoreArray(sortedByTime)
-        console.log("time", scoreArray)
-    }
-
     return (
         <section className="scores">
             <h2>SCORES</h2>
             <div className="scores-header">
-                <button onClick={renderByRolls}>ROLLS</button>
-                <button onClick={renderByTime}>TIME</button>
+                <button onClick={() => setScoreArray(sortedByRolls)}>ROLLS</button>
+                <button onClick={() => setScoreArray(sortedByTime)}>TIME</button>
             </div>
 
             {scoreArray.map((score, index) => {
