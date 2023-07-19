433f48986ed4b501f07221b5c8795378de01b074
vishalicious213 <vishalicious213@gmail.com>
2023-07-19 12:22:50 -0700

    updated sortedByTime and sortedByRolls. changed rolls/time columns titles to buttons. using them to sort and render

--- src/Scores.jsx
+++ src/Scores.jsx
@@ -1,14 +1,34 @@
+import { useState } from "react"
+
 export default function Scores({scores}) {
-    const sortedByTime = scores.sort(function(a, b){return a.time - b.time})
-    const sortedByRolls = scores.sort(function(a, b){return a.roll - b.roll})
+    const [scoreArray, setScoreArray] = useState(scores)
+    const sortedByRolls = [...scoreArray].sort((a, b) => a.roll - b.roll)
+    const sortedByTime = [...scoreArray].sort((a, b) => a.time - b.time)
+
+    console.log("sortedByRolls", sortedByRolls)
+    console.log("sortedByTime", sortedByTime)
+
+    console.log("scores", scores)
+
+    function renderByRolls() {
+        setScoreArray(sortedByRolls)
+        console.log("rolls", scoreArray)
+    }
+
+    function renderByTime() {
+        setScoreArray(sortedByTime)
+        console.log("time", scoreArray)
+    }
+
     return (
         <section className="scores">
             <h2>SCORES</h2>
             <div className="scores-header">
-                <div>ROLLS</div>
-                <div>TIME</div>
+                <button onClick={renderByRolls}>ROLLS</button>
+                <button onClick={renderByTime}>TIME</button>
             </div>
-            {sortedByTime.map((score, index) => {
+
+            {scoreArray.map((score, index) => {
                 return (
                     <div className="scoreboard" key={index}>
                         <div>{score.roll}</div>
