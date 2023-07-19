938f3deaa4d63ef92594874babfb83f4332a343f
vishalicious213 <vishalicious213@gmail.com>
2023-07-19 11:37:57 -0700

    initial layout for scoreboard

--- src/Scores.jsx
+++ src/Scores.jsx
@@ -2,9 +2,16 @@ export default function Scores({scores}) {
     return (
         <section className="scores">
             <h2>SCORES</h2>
+            <div className="scores-header">
+                <div>ROLLS</div>
+                <div>TIME</div>
+            </div>
             {scores.map((score, index) => {
                 return (
-                    <div key={index}>Rolls: {score.roll} | Time: {score.time}</div>
+                    <div className="scoreboard" key={index}>
+                        <div>{score.roll}</div>
+                        <div>{score.time}s</div>
+                    </div>
                 )
             })}
         </section>
